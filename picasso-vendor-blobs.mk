64a4cdd8b2db731f85559ad5f295ab979de4561a
Kris ~ <waaydownsouth@gmail.com>
2012-04-19 08:17:02 +1200

    paragon NTFS driver to proprietary

--- picasso-vendor-blobs.mk
+++ picasso-vendor-blobs.mk
@@ -1,13 +1,13 @@
 # Copyright (C) 2011 The CyanogenMod Project
 #
-# Licensed under the Apache License, Version 2.0 (the \"License\");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an \"AS IS\" BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -98,5 +98,5 @@ PRODUCT_COPY_FILES += \
     vendor/acer/picasso/proprietary/lib/libnvwinsys.so:/system/lib/libnvwinsys.so \
     vendor/acer/picasso/proprietary/lib/libnvwsi.so:/system/lib/libnvwsi.so \
     vendor/acer/picasso/proprietary/lib/libstagefrighthw.so:/system/lib/libstagefrighthw.so \
+    vendor/acer/picasso/proprietary/lib/modules/ufsd.ko:/system/lib/modules/ufsd.ko \
     vendor/acer/picasso/proprietary/lib/omxplayer.so:/system/lib/omxplayer.so \
-
